35ddaa28bdf1352567987c2240d84b15724feec2
“AccelQuasarDragon” <138998466+AccelQuasarDragon@users.noreply.github.com>
2025-02-10 00:40:22 -0800

    init commit

---
+++ example1.py
@@ -0,0 +1,15 @@
+class Legend():
+    name = "Default Name"
+    health = 100
+    skills = {"default_skill": 10}
+    alias = ["Default Alias"]
+
+Magenetguy_class = Legend()
+Magenetguy_class.name = "Magenetguy"
+Magenetguy_class.health = 675
+Magenetguy_class.skills = {"sword_slash":75}
+Magenetguy_class.alias = ["Solenoid", "Magnet", "Electro"]
+
+Grasshopper_class = Legend()
+
+breakpoint()
