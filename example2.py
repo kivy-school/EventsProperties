e177182617963a706b4c2b35f9eda0666d6125ce
“AccelQuasarDragon” <138998466+AccelQuasarDragon@users.noreply.github.com>
2025-02-10 01:55:56 -0800

    u

---
+++ example2.py
@@ -0,0 +1,49 @@
+class Legend():
+    _name = "Default Name" #initial default value
+    _health = 100
+    skills = {"default_skill": 10}
+    alias = ["Default Alias"]
+
+    def __init__(self, *args):
+        self._health = self.health 
+
+    def _get_name(self):
+        print(f"This Legend's name is {self._name}")
+        return self._name
+    
+    def _set_name(self, new_name):
+        self._name = new_name
+        print(f"This Legend name is now {self._name}")
+        return self._name
+    
+    def _del_name(self):
+        print("This Legend will be nameless!")
+        del self._name
+
+    name = property(
+        fget=_get_name,
+        fset=_set_name,
+        fdel=_del_name,
+        doc="This is the name of the Legend."
+    )
+
+    @property
+    def health(self):
+        "This is the health property of the Legend."
+        print("This is the current health")
+        return self._health
+    
+    @health.setter
+    def health(self, new_health):
+        print(f"This is the new health {new_health}")
+        self._health = new_health
+        return self._health
+
+    @health.deleter
+    def health(self):
+        print("Legend health will be set to default!")
+        del self._health
+
+print("to get the docstring:", Legend.name.__doc__)
+
+breakpoint()
