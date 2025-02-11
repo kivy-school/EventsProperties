d73f204343cc34574d985d460e193fa059ed5116
“AccelQuasarDragon” <138998466+AccelQuasarDragon@users.noreply.github.com>
2025-02-11 14:58:46 -0800

    update, example4 is kinda hard to make an easy version and to do tracking WITHOUT mixing widgets and data to follow mvvm

--- example4_thinking.py
+++ example4_thinking.py
@@ -4,7 +4,6 @@ from kivy.lang import Builder
 from kivy.app import App
 from kivy.core.window import Window
 
-
 def stringerr_handler(*args):
     print(f"string error argument {args} is handled!")
     return str(args[0])
@@ -30,10 +29,51 @@ brick_frog = KivyLegend(
     )
 
 kv = """
-BoxLayout:
+#:import bat_brick example4.bat_brick
+
+ColoredBox:
+    bg_color: 0,0,0,0
     orientation: "horizontal"
-    BoxLayout:
-        text: "Hello World"
+    ColoredBox:
+        orientation: "vertical"
+        bg_color: 1,0,0,0
+        Button:
+            text: "Brick_Frog"
+            on_release: 
+                print("What is root?", root)
+        LabelB:
+            text: "Brick_Frog health"
+    ColoredBox:
+        orientation: "vertical"
+        bg_color: 0,1,0,0
+        Button:
+            text: "Brick_Frog attack"
+        Button:
+            text: "Bat_Brick attack"
+    ColoredBox:
+        orientation: "vertical"
+        bg_color: 0,0,1,0
+        Button:
+            text: bat_brick.name
+        LabelB:
+            text: "Bat_Brick health"
+
+
+<ColoredBox@BoxLayout>:
+	bg_color: 1,1,1,1
+	canvas.before: 
+		Color:
+			rgba: self.bg_color
+		Rectangle:
+			pos: self.pos
+			size: self.size
+
+<LabelB@Button>:
+	background_normal: ''
+    background_down: ''
+    background_disabled_normal: ''
+    background_disabled_down: ''
+    background_color: 0, 0, 0, 0
 """
 
 class MainApp(App):
